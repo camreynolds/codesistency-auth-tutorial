6f1968c55b96b2358ef5d6b0de46cfcbe6f567ed
Carlos A Muñoz R <camr.personal@gmail.com>
2026-09-26 00:15:00 -0500

    Building a welcome email template.

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,20 @@
 //const express = requires("express");
 import express from "express";
 import dotenv from "dotenv";
+
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
-  res.send("Hello world!")
-});
+app.use(express.json()); // allows us to parse incoming request:req.body  
 
-app.listen(3000,()=>{
+app.use("/api/auth/", authRoutes);
+
+app.listen(PORT,()=>{
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port:",PORT);
 });
